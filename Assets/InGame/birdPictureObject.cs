4d705f8dfc9d72747bec17301c142af075140fd2
green-Desktop <appieosugood@gmail.com>
2023-12-28 00:23:29 +0800

    ground picture

---
+++ Assets/InGame/birdPictureObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace InGame
+{
+    internal enum birdType
+    {
+        bird1,
+        bird2,
+        bird3,
+        bird4
+    }
+
+    internal enum birdState
+    {
+        stand,
+        fly
+    }
+
+    [CreateAssetMenu(fileName = "birdPicture", menuName = "pirdPicture", order = 0)]
+    public class birdPictureObject : ScriptableObject
+    {
+        public Sprite[][] birdPicture;
+    }
+}
