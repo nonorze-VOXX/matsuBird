7e963a799cf055e85c726f7cf7a550308d22a404
wj4wj4 <wj4wj461@gmail.com>
2024-01-02 01:02:43 +0800

    complete gacha

---
+++ Assets/OutGame/GachaPage1.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class GachaPage1 : Scene_manerger
+{
+    private GachaView _gachaView;
+    private bool IsBirdComed = false;
+
+    private List<string> birdPath = new List<string>()
+    {
+        "b1",
+        "b2",
+        "b3",
+        "b4"
+    };
+    public  override void Start()
+    {
+        _gachaView = GameObject.FindObjectOfType<GachaView>();
+        _gachaView.kami.onClick.AddListener(PlayKasu);
+        _gachaView.backButton.onClick.AddListener(BackMain);
+        Debug.Log(_gachaView.backButton);
+    }
+
+    public override void Update()
+    {
+        
+    }
+    
+    void changeTostoImage()
+    {
+        if (_gachaView.pan != null && _gachaView.tostoList.Length > 0 && !IsBirdComed)
+        {
+            int randomIndex = Random.Range(0, _gachaView.tostoList.Length);
+            Sprite tostoImage = _gachaView.tostoList[randomIndex];
+            if (randomIndex == 1)
+            {
+                _gachaView.birdCome.Play();
+                OutGameManager.instance.AddBirdList(RandomBird());
+                _gachaView.panKasu.gameObject.SetActive(false);
+                IsBirdComed = true;
+            }
+
+            _gachaView.pan.sprite = tostoImage;
+        } 
+    }
+
+    TeamBird RandomBird()
+    {
+        int randomNumber = Random.Range(1, 5);
+        string path = birdPath[randomNumber-1];
+        _gachaView.bird.sprite = Resources.Load<Sprite>("Image/bird/" + path + "_f");
+        
+        return new TeamBird(path,randomNumber);
+    }
+    
+    void PlayKasu()
+    {
+        _gachaView.panClick.Play();
+        changeTostoImage();
+        _gachaView.panKasu.Play();
+    }
+
+    void BackMain()
+    {
+        Debug.Log("ho to main");
+        OutGameManager.instance.GotoScene("Main");
+    }
+
+}
